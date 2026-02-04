684294fd898a508d152eb530c431c6c1c9ad314b
Felix Wahl <47465895+goldbarth@users.noreply.github.com>
2026-02-04 17:50:34 +0100

    feat: implement PlaylistService with CRUD operations and video management

---
+++ DevToolbox/Features/YouTubePlayer/Service/IPlaylistService.cs
@@ -0,0 +1,17 @@
+﻿using DevToolbox.Features.YouTubePlayer.Models;
+
+namespace DevToolbox.Features.YouTubePlayer.Service;
+
+public interface IPlaylistService
+{
+    Task<List<Playlist>> GetAllPlaylistsAsync();
+    Task<Playlist?> GetPlaylistByIdAsync(Guid playlistId);
+    
+    Task CreatePlaylistAsync(Playlist playlist);
+    Task UpdatePlaylistAsync(Playlist playlist);
+    Task DeletePlaylistAsync(Guid playlistId);
+    
+    Task AddVideoToPlaylistAsync(Guid playlistId, VideoItem video);
+    Task RemoveVideoFromPlaylistAsync(Guid playlistId, Guid videoId);
+    Task UpdateVideoPositionsAsync(Guid playlistId, List<VideoItem> videos);
+}
