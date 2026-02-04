5e272dd84fe2669120ca4c8787ae28321b07370f
Felix Wahl <47465895+goldbarth@users.noreply.github.com>
2026-02-04 17:49:24 +0100

    feat: set up ApplicationDbContext with Playlist and VideoItem entities, include migrations for EF Core setup

---
+++ DevToolbox/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevToolbox.Data.EntityMapping;
+using DevToolbox.Features.YouTubePlayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevToolbox.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Playlist> Playlists { get; set; }
+    public DbSet<VideoItem> VideoItems { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PlaylistMapping());
+        modelBuilder.ApplyConfiguration(new VideoItemMapping());
+    }
+}
